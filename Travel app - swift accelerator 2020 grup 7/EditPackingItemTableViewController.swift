af538050101216a3fa3b765c01b76c6f57d09270
Rochelle <rochelle_teh_xinyue@temasekjc.moe.edu.sg>
2020-11-28 18:20:26 +0800

    came up with the EditPackingItemTableViewController (very long name yes) and then did a performSegue to it from the packing list table vc

---
+++ Travel app - swift accelerator 2020 grup 7/EditPackingItemTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EditPackingItemTableViewController.swift
+//  Travel app - swift accelerator 2020 grup 7
+//
+//  Created by rochelletxy on 28/11/20.
+//
+
+import UIKit
+
+class EditPackingItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+}
--- Travel app - swift accelerator 2020 grup 7/PackingListTableViewController.swift
+++ Travel app - swift accelerator 2020 grup 7/PackingListTableViewController.swift
@@ -46,6 +46,10 @@ class PackingListTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: self)
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
