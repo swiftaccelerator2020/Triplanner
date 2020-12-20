df6c9f773e87c49e558c5a54c942e03fea21010c
Yu Youyou <yuyouyou.k@outlook.com>
2020-12-20 10:03:07 +0800

    it has been weird... solved one more budget data storage issue

--- Travel app - swift accelerator 2020 grup 7/BudgetTableViewController.swift
+++ Travel app - swift accelerator 2020 grup 7/BudgetTableViewController.swift
@@ -11,7 +11,7 @@ class BudgetTableViewController: UITableViewController {
     
     var trips: [Trip] = []
     var tripNo: Int = 0
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = viewTitle
@@ -21,37 +21,37 @@ class BudgetTableViewController: UITableViewController {
         }
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return spendingItemsArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "budgetCell", for: indexPath)
         cell.textLabel?.text = "\(String(spendingItemsArray[indexPath.row].cost))"
         cell.detailTextLabel?.text = spendingItemsArray[indexPath.row].name
-
-
+        
+        
         return cell
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -64,46 +64,46 @@ class BudgetTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
         
-//        for (key, value) in storageSpendingItemsDict{
-//            if spendingItemsArray.count > indexPath.row{
-//                if key == spendingItemsArray[indexPath.row].category{
-//                    storageSpendingItemsDict[key] = []
-//                    for i in spendingItemsArray{
-//                        if i.category == key{
-//                            storageSpendingItemsDict[key]?.append(i)
-//                        }
-//                    }
-//                }
-//            }else{
-//                storageSpendingItemsDict[key]?.removeLast()
-//            }
-//            trips[tripNo].budget = storageSpendingItemsDict
-//            Trip.saveToFile(trips: trips)
-//        }
+        //        for (key, value) in storageSpendingItemsDict{
+        //            if spendingItemsArray.count > indexPath.row{
+        //                if key == spendingItemsArray[indexPath.row].category{
+        //                    storageSpendingItemsDict[key] = []
+        //                    for i in spendingItemsArray{
+        //                        if i.category == key{
+        //                            storageSpendingItemsDict[key]?.append(i)
+        //                        }
+        //                    }
+        //                }
+        //            }else{
+        //                storageSpendingItemsDict[key]?.removeLast()
+        //            }
+        //            trips[tripNo].budget = storageSpendingItemsDict
+        //            Trip.saveToFile(trips: trips)
+        //        }
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         
     }
-
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let navigationVC = segue.destination as? UINavigationController{
             if let dest = navigationVC.topViewController as? AddSpendingViewController{
@@ -121,14 +121,14 @@ class BudgetTableViewController: UITableViewController {
         }
         
         if segue.destination is BudgetViewController{
-//            for i in self.spendingItemsArray{
-//                if self.spendingItemsDict[i.category] == nil{
-//                    spendingItemsDict[i.category] = [i]
-//                }else{
-//                    spendingItemsDict[i.category]?.append(i)
-//                }
-//                print("this is thing", spendingItemsDict)
-//            }
+            //            for i in self.spendingItemsArray{
+            //                if self.spendingItemsDict[i.category] == nil{
+            //                    spendingItemsDict[i.category] = [i]
+            //                }else{
+            //                    spendingItemsDict[i.category]?.append(i)
+            //                }
+            //                print("this is thing", spendingItemsDict)
+            //            }
             spendingItemsDict = storageSpendingItemsDict
         }
     }
@@ -140,20 +140,30 @@ class BudgetTableViewController: UITableViewController {
             if let source = segue.source as? AddSpendingViewController{
                 if source.isExistingItem == true{
                     self.spendingItemsArray[currentSpendingNo] = source.budgetItem!
-                    storageSpendingItemsDict[source.budgetItem!.category]![currentSpendingNo] = source.budgetItem!
+                    storageSpendingItemsDict[source.budgetItem!.category]?[currentSpendingNo] = source.budgetItem!
+                    print("if", storageSpendingItemsDict)
                 }else{
-                    self.spendingItemsArray.append( source.budgetItem!)
-                    storageSpendingItemsDict[source.budgetItem!.category]!.append(source.budgetItem!)
-                    print(spendingItemsArray)
+                    self.spendingItemsArray.append(source.budgetItem!)
+                    if storageSpendingItemsDict[source.budgetItem!.category] == nil{
+                        print("what the heck:",source.budgetItem!.category)
+                        let tempCat = source.budgetItem!.category
+                        storageSpendingItemsDict[tempCat] = [source.budgetItem!]
+                        print("else if", storageSpendingItemsDict)
+                    }else{
+                        storageSpendingItemsDict[source.budgetItem!.category]?.append(source.budgetItem!)
+                        print("else else", storageSpendingItemsDict)
+                    }
+                    print("spendingItemsArray:", spendingItemsArray)
+                    print("storageDict:", storageSpendingItemsDict)
                 }
-
-//                for i in self.spendingItemsArray{
-//                    if self.storageSpendingItemsDict[i.category] == nil{
-//                        storageSpendingItemsDict[i.category] = [i]
-//                    }else{
-//                        storageSpendingItemsDict[i.category]?.append(i)
-//                    }
-//                }
+                
+                //                for i in self.spendingItemsArray{
+                //                    if self.storageSpendingItemsDict[i.category] == nil{
+                //                        storageSpendingItemsDict[i.category] = [i]
+                //                    }else{
+                //                        storageSpendingItemsDict[i.category]?.append(i)
+                //                    }
+                //                }
                 trips[tripNo].budget = storageSpendingItemsDict
                 Trip.saveToFile(trips: trips)
                 
@@ -161,7 +171,7 @@ class BudgetTableViewController: UITableViewController {
             tableView.reloadData()
         }
     }
-    }
-    
+}
+
 
 
